5c3ef75519646426e0e657076c1f1e6670a9db27
swp91 <seongwoo4477@gmail.com>
2023-08-09 15:33:31 +0900

    fix #14 import버그 수정

---
+++ src/components/FilterModalBackdrop.tsx
@@ -0,0 +1,8 @@
+import React from "react";
+import { ModalBackgorund } from "./stFilterModal";
+
+const FilterModalBackdrop = () => {
+  return <ModalBackgorund></ModalBackgorund>;
+};
+
+export default FilterModalBackdrop;
